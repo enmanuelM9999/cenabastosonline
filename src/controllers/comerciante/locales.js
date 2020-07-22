638a73e7765bfa54116ba402cbe6e1b7042f2553
enmanuelM <enmanuelcamilo9999@gmail.com>
2020-07-22 17:51:58 -0400

    commit

--- src/controllers/comerciante/locales.js
+++ src/controllers/comerciante/locales.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const { esComerciante,esComercianteAprobado } = require('../../lib/auth');
+const pool = require("../../database");
 
+
+router.get('/crearLocal', esComercianteAprobado, async (req, res) => {
+    const rowsProductos= await pool.query("SELECT pkIdProducto, nombreProducto FROM producto ORDER BY nombreProducto ASC");
+    req.render("comerciante/locales/crearLocal",{rowsProductos});
+});
 
 router.post('/crearLocal', esComercianteAprobado, async (req, res) => {
+   const {name,descripcion,tipoLocal,productos}=req.body;
     console.log(req.body);
+
+    const resultPersonaJuridica = await pool.query("INSERT INTO personaJuridica VALUES ()");
+    const idPersonaJuridica = resultPersonaJuridica.insertId;
+
+    let newLocalComercial = {
+
+    };
     res.redirect('/comerciante/index');
 });
 
 
 
 module.exports = router;
+
