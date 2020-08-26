6f61bd93b5d00aca5f0e5dce67c1d15829ab59ed
enmanuelM <enmanuelcamilo9999@gmail.com>
2020-08-26 18:54:03 -0500

    to heroku

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const pool= require("../database");
 
 router.get('/', async (req, res) => {
-    res.render('index');
+    try {
+        let rowsAdmin= await pool.query("SELECT horaAperturaMayorista,horaCierreMayorista,horaAperturaMinorista,horaCierreMinorista FROM admin");
+        let moment=require("moment");
+        rowsAdmin[0].horaAperturaMayorista= moment(rowsAdmin[0].horaAperturaMayorista,"HH:mm").format("LT").toString();
+        rowsAdmin[0].horaCierreMayorista= moment(rowsAdmin[0].horaCierreMayorista,"HH:mm").format("LT").toString();
+        rowsAdmin[0].horaAperturaMinorista= moment(rowsAdmin[0].horaAperturaMinorista,"HH:mm").format("LT").toString();
+        rowsAdmin[0].horaCierreMinorista= moment(rowsAdmin[0].horaCierreMinorista,"HH:mm").format("LT").toString();
+        res.render('index',{rowsAdmin:rowsAdmin[0]});
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 router.get('/enlaces', async (req, res) => {
